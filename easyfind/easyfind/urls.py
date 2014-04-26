a9cf1ba431b009539a1114c18cbba3d8643722a6
Dogukan Tufekci <dogukan@creco.co>
2014-04-27 00:06:29 +0300

    fixed bugs

--- easyfind/easyfind/urls.py
+++ easyfind/easyfind/urls.py
@@ -15,8 +15,6 @@ urlpatterns = patterns('',
     url(r'^api/jobs/', include('jobs.urls', namespace='jobs', app_name='jobs')),
     url(r'^api/notifications/', include('notifications.urls', namespace='notifications', app_name='notifications')),
     url(r'^api/offers/', include('offers.urls', namespace='offers', app_name='offers')),
-    url(r'^api/sellers/', include('sellers.urls', namespace='sellers', app_name='sellers')),
-    url(r'^api/transactions/', include('transactions.urls', namespace='transactions', app_name='transactions')),
 
     # django admin
     url(r'^admin/', include(admin.site.urls))
