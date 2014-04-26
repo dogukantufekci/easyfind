61204dd0ca9ef0eab0fde0ce995dab77b51458de
Veysel Sahin <thevyxx@gmail.com>
2014-04-26 19:01:48 +0300

    admin modelleri

--- easyfind/easyfind/s3utils.py
+++ easyfind/easyfind/s3utils.py
@@ -2,3 +2,9 @@ from storages.backends.s3boto import S3BotoStorage
 
 StaticS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaS3BotoStorage = lambda: S3BotoStorage(location='media')
+
+import math
+
+
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
