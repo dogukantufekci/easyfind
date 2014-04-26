ac025c286c66437f99f738cdc270d1adadd5ca9d
Dogukan Tufekci <dogukan@creco.co>
2014-04-26 18:49:47 +0300

    fixed models

--- easyfind/offers/models.py
+++ easyfind/offers/models.py
@@ -1,5 +1,3 @@
-from geoposition.fields import GeopositionField
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -14,8 +12,9 @@ class Offer(AbstractModel):
 
     job = models.ForeignKey('jobs.Job', related_name='offers', related_query_name='offer', verbose_name=_("Job"))
     seller = models.ForeignKey('sellers.Seller', related_name='offers', related_query_name='offer', verbose_name=_("Seller"))
-    price = models.DecimalField(decimal_places=PRICE_DECIMAL_PLACES, max_digits=PRICE_MAX_DIGITS, verbose_name=_("Discounted Price"))
+    price = models.DecimalField(decimal_places=PRICE_DECIMAL_PLACES, max_digits=PRICE_MAX_DIGITS, verbose_name=_("Price"))
     status = models.PositiveIntegerField(choices=Statuses.CHOICES, default=Statuses.WAITING,verbose_name=_("Status"))
+
 
     def __unicode__(self):
         return _(u"{seller}'s offer for Job {job} is {price}").format(seller=self.seller.user.get_full_name(), job=self.job.id, price=self.price)
